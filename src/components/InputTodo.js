04abc7b10b7738d6a70353c3ed7e9589a246ad3d
Ben Kiboma <omayioben@gmail.com>
2022-07-02 22:36:01 +0300

    Add text input

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -4,10 +4,20 @@ class InputToDo extends Component {
   state = {
     title: ""
   };
+
+  onChange = e => {
+    console.log("Input...")
+  };
+
   render() {
     return (
       <form>
-        <input type='text' placeholder="Add to do ..." value={this.state.title} />
+        <input
+         type='text'
+         placeholder="Add to do ..."
+         value={this.state.title}
+         onChange={this.onChange}
+         />
         <button>Submit</button>
       </form>
     )
