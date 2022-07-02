84115e743e9e9d8ec9024e6cc4d630da63727e6c
Ben Kiboma <omayioben@gmail.com>
2022-07-02 14:49:53 +0300

    Create toDo container

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const element = <h1>This is my second react project from scratch</h1>
-ReactDOM.render(element, document.getElementById('root'));
+import ToDoContainer from "./components/TodoContainer";
+
+//const element = <h1>This is my second react project from scratch</h1>
+
+ReactDOM.render(<ToDoContainer/>, document.getElementById('root'));
+
