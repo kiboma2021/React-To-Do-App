e1d175ca0efdd3111a6d5d2d067024e61062bc08
Ben Kiboma <omayioben@gmail.com>
2022-07-02 16:12:28 +0300

    Add todo title

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,10 +1,37 @@
 import React from "react";
 class ToDoContainer extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Setup development environment",
+        completed: true
+      },
+      {
+        id: 2,
+        title: "Develop website and add content",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Deploy to live server",
+        completed: false
+      }
+    ]
+  };
   render() {
     return (
       <React.Fragment> 
         <h1> Hello. Welcome to my to do app</h1>
         <p>This is the best App in the market</p>
+        <ul>
+          {this.state.todos.map(
+            todo => (
+              <li>{todo.title} </li>
+            )
+          )}
+        </ul>
+
       </React.Fragment>
     )
   }
