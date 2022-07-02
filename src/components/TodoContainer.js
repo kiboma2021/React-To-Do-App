b96f736fd3bcb3afdf2b0692ebd6c85229e000bc
Ben Kiboma <omayioben@gmail.com>
2022-07-02 14:47:30 +0300

    Create toDo container

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+class ToDoContainer extends React.Component {
+  render() {
+    return (
+      <div> 
+        <h1> Hello. Welcome to my to do app</h1>
+        <p>This is the best App in the market</p>
+      </div>
+    )
+  }
+}
+
+export default ToDoContainer;
