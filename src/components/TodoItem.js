47ea6d8a0a818b820c5566f7f44c0d4e7145b2ea
Ben Kiboma <omayioben@gmail.com>
2022-07-03 10:37:33 +0300

    Enable editing

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,19 +1,42 @@
 import React from "react";
 import styles from "./TodoItem.module.css"
 class TodoItem extends React.Component {
+  state = {
+    editing: false,
+  }
+  handleEditting = () => {
+    this.setState ({
+      editing: true
+    })
+  }
   render(){
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#595959",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    }
+    const { completed, id, title } = this.props.todo
+
+    
+
     return (
       <li className={styles.item}>
-        <input 
-          type='checkbox'
-          className={styles.checkbox}
-          checked={this.props.todo.completed} 
-          onChange={() => this.props.handleChangeProps(this.props.todo.id)} 
-        /> 
-        <button onClick={() => this.props.delToDoProps(this.props.todo.id) }>
-          Delete
-        </button>
-        {this.props.todo.title}
+        <div onDoubleClick={this.handleEditting}>
+          <input 
+            type='checkbox'
+            className={styles.checkbox}
+            checked={completed} 
+            onChange={() => this.props.handleChangeProps(id)} 
+          /> 
+          <button onClick={() => this.props.delToDoProps(id) }>
+            Delete
+          </button>
+          <span style={completed ? completedStyle : null}>
+              {title}
+          </span>
+        </div>
+        <input type="text" className={styles.textInput} />
       </li>
     )
   }
