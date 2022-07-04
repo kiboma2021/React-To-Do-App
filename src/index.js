2626428417b25b8c3ec2ec0a9199d467fa352283
Ben Kiboma <omayioben@gmail.com>
2022-07-05 00:21:16 +0300

    Add React Rooter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import ToDoContainer from "./functionBased/components/TodoContainer";
 import "./functionBased/App.css"
+import { BrowserRouter as Router } from "react-router-dom"
 
 ReactDOM.render(
-  <React.StrictMode> 
-    <ToDoContainer/>
+  <React.StrictMode>
+    <Router>
+      <ToDoContainer/>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
